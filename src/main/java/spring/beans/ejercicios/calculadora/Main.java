50477c9640375b5959f733fa5e2a2167a9d7e6dc
loprw <loprwdemileto@gmail.com>
2025-05-26 20:04:33 +0200

    Clase 26/05/2025
    Se crean/modifican:
    
    - Se modifica la clase Main.java en
    .spring.benas.ejercicios.calculadora.

--- src/main/java/spring/beans/ejercicios/calculadora/Main.java
+++ src/main/java/spring/beans/ejercicios/calculadora/Main.java
@@ -9,6 +9,9 @@ public class Main {
 		
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		Controlador contr = (Controlador) context.getBean("controller");
+		Numeros nums = context.getBean(Numeros.class);
+		nums.setNum1(9);
+		nums.setNum2(8);
 		System.out.println(contr.ejecutarOperacion());
 	}
 
